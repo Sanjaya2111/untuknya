4845dd95bfc193ad8d0a26c79305964a27606340
Sanjaya2111 <158828650+Sanjaya2111@users.noreply.github.com>
2024-02-12 22:55:10 +0700

    Add files via upload

---
+++ AskHer.js
@@ -0,0 +1,34 @@
+const qs = (selector) => document.querySelector(selector);
+const question = qs(".question");
+const gif = qs(".gif");
+const [yesBtn, noBtn] = [".yes-btn", ".no-btn"].map(qs);
+
+const handleYesClick = () => {
+  question.innerHTML = "Yeahhhhhhhhhhh! See you tomorrow!!";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+
+  // Remove the 'mouseover' event listener from noBtn
+  noBtn.removeEventListener("mouseover", handleNoMouseOver);
+
+  // Remove the noBtn from the DOM
+  noBtn.remove();
+
+  // Replace yesBtn with the new letsGoBtn
+  yesBtn.replace();
+};
+
+const handleNoMouseOver = () => {
+  const { width, height } = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - width;
+  const maxY = window.innerHeight - height;
+
+  noBtn.style.left = `${Math.floor(Math.random() * maxX)}px`;
+  noBtn.style.top = `${Math.floor(Math.random() * maxY)}px`;
+};
+
+yesBtn.addEventListener("click", handleYesClick);
+noBtn.addEventListener("mouseover", handleNoMouseOver);
+
+yesBtn.addEventListener('click', () => {
+  window.location.href = 'code ucapan valentine.html'
+})
